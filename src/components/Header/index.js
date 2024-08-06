432e9f94098c1f12ca7b308e543be29e8e6d0efd
Luiz Gabriel <luiz.figueiredo@aluno.ifsp.edu.br>
2024-08-06 08:47:14 -0300

    06/08

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+const header = () => {
+    return (
+        <header>
+            <div className='header center'>
+                <picture>
+                    <img src='./assets/logo.jpg' alt='logo hamburgueria'></img>
+                </picture>
+
+                <nav>
+                    <ul>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
