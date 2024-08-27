31b3d5b6485a05be790855f6f9fbee639aae5ba0
Luiz Gabriel <luiz.figueiredo@aluno.ifsp.edu.br>
2024-08-27 08:50:36 -0300

    '2024-08-27'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './style.css';
 
-const header = () => {
+const Header = () => {
     return (
         <header>
             <div className='header center'>
                 <picture>
-                    <img src='./assets/logo.jpg' alt='logo hamburgueria'></img>
+                    <img src='./assets/logo.webp' alt='logo hamburgueria'></img>
                 </picture>
 
                 <nav>
                     <ul>
-                        
+                        <li>
+                            <a href='#'>Produtos</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>Sobre</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>Contato</a>
+                        </li>
                     </ul>
                 </nav>
             </div>
         </header>
     )
 }
+
+export default Header;
---
+++ src/components/SecaoCapa/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../SecaoCapa/style.css'
+
+const Header = () => {
+    return (
+        <section className='secao-capa'>
+            <div className='capa center'>
+                <div className='texto capa'>
+                    <p>Niedrige Preise auf</p>
+                    <h1>Burgers, fries, frozen custards, shakes and alcohol</h1>
+                    <p>你在这里找到它!</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default SecaoCapa
